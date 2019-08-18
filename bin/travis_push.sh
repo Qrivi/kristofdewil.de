4d521527c3ded2e79b45b6e0d64b3d96aa970142
Build Boy <travis@krivi.be>
2019-08-19 01:49:00 +0200

    Travis build:

--- bin/travis_push.sh
+++ bin/travis_push.sh
@@ -2,9 +2,15 @@
 #!/bin/sh
 
 push_build() {
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" --author "Build Boy <travis@krivi.be>"
-  git push --quiet
+  if [[ $(git log -1 | cat) == *"Travis build"* ]]
+  then
+    echo "Last commit was a Travis build. Not taking any action."
+  else
+    echo "Last commit was a code change. Pushing the build."
+    git add .
+    git commit --author "Build Boy <travis@krivi.be>" --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git push --quiet
+  fi
 }
 
 push_build
