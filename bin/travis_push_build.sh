a11f02c92cc1cdb1722739c4fd1e53d4df8b54fa
Kristof Dewilde <git@krivi.be>
2019-08-19 11:39:16 +0200

    Generalize git url

--- bin/travis_push_build.sh
+++ bin/travis_push_build.sh
@@ -3,17 +3,17 @@
 push_build() {
   git checkout master
 
-  if [[ $(git log -1 | cat) == *"Travis build"* ]]
+  if [[ $TRAVIS_COMMIT_MESSAGE == *"Travis production build"* ]]
   then
     echo "Last commit was a Travis build. Not taking any action."
   else
     echo "Last commit was a code change. Pushing this build."
 
     git remote rm origin
-    git remote add origin https://Qrivi:${GH_TOKEN}@github.com/Qrivi/website.git > /dev/null 2>&1
+    git remote add origin https://Qrivi:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 
     git add .
-    git commit --author "Build Boy <travis@krivi.be>" --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git commit --author "Build Boy <travis@krivi.be>" --message "Travis production build: $TRAVIS_BUILD_NUMBER"
     git push origin master --quiet > /dev/null 2>&1
   fi
 }
