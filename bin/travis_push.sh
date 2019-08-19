ffe55dd3b3853f56daf8c63b679ed7252ab35d1e
Kristof Dewilde <git@krivi.be>
2019-08-19 02:25:58 +0200

    try pushing again

--- bin/travis_push.sh
+++ bin/travis_push.sh
@@ -2,14 +2,20 @@
 #!/bin/sh
 
 push_build() {
+  git checkout master
+
   if [[ $(git log -1 | cat) == *"Travis build"* ]]
   then
     echo "Last commit was a Travis build. Not taking any action."
   else
     echo "Last commit was a code change. Pushing the build."
+
+    git remote rm origin
+    git remote add origin https://Qrivi:${GH_TOKEN}@github.com/Qrivi/website.git > /dev/null 2>&1
+
     git add .
     git commit --author "Build Boy <travis@krivi.be>" --message "Travis build: $TRAVIS_BUILD_NUMBER"
-    git push --quiet
+    git push origin master --quiet > /dev/null 2>&1
   fi
 }
 
