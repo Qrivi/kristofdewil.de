0310f0f5f3b61a778eeca6ddad8a550f43586654
Kristof Dewilde <kristof.dewilde@intix.eu>
2019-12-19 17:55:16 +0100

    Ported CSS

--- assets/dist/bundle.js
+++ assets/dist/bundle.js
@@ -162,7 +162,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60114" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34329" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
